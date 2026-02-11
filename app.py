6eecca32919d8d543d46e658a6a26d020e117bc4
vineetkumar58 <vineet123koli@gmail.com>
2026-02-11 20:05:57 +0530

    updated message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Docker Deployed App!"
+    return "Hello from intelligent ci/cd system App!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
